e435039827fec7e09f1805cef971c4b77334a223
JeffersonAlves7 <ilj031420@hotmail.com>
2022-06-29 17:50:50 -0300

    Adicionada a visualização da quantidade embalado/total no componente ListaAtiva

--- src/App/Views/ListaAtiva/index.jsx
+++ src/App/Views/ListaAtiva/index.jsx
@@ -1,5 +1,7 @@
 import Button from "../../Components/Buttons/WmsButton/index"
 import scanPedido from "../../api/modules/scanPedido"
+import { useState, useEffect } from "react"
+import api from "../../api"
 
 function Scanner() {
     return (
@@ -8,11 +10,28 @@ function Scanner() {
 }
 
 export default (props) => {
+    const [info, setInfo] = useState({ embalado: 0, total: 0 })
+
+    useEffect(() => {
+        api.get('/pedidos?idLista=' + props.id)
+            .then(res => {
+                const { data } = res
+                const { response } = data
+                const embalado = response.filter(pedido => pedido.situacao == "embalado").length
+                const total = response.filter(pedido => pedido.situacao != "embalado").length
+                setInfo({
+                    embalado: embalado,
+                    total: total
+                })
+            })
+
+    })
 
     return (
         <>
-            <header className='border-b-2 p-2 border-b-wmsPink w-full'>
+            <header className='border-b-2 p-2 border-b-wmsPink w-full flex justify-between items-center'>
                 <h1 className="text-2xl font-bold">Lista de Coleta Ativa: {props.id}</h1>
+                <p>{info.embalado}/{info.total}</p>
             </header>
             <main className="flex gap-10 items-center">
                 <Scanner />
